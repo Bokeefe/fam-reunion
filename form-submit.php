604020de23885eaa332e93f2d438a0c0d9c6c0b3
Brendan O'Keefe <brndnokf@yahoo.com>
2017-08-22 07:47:16 -0600

    tues

---
+++ form-submit.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+//
+//
+//        $to      = "brendanokeefe96@gmail.com";
+//        $subject = "New Swift-Carr Registration";
+//        $body    ="testing";
+//
+//        $success = mail($to, $subject, $body);
+//
+//        if ($success) {
+//            echo json_encode(['status' => 1]);
+//
+//        } else {
+//            echo json_encode(['status' => 0]);
+//        }
+//    } else {
+//        echo json_encode(['status' => 0]);
+
+}
